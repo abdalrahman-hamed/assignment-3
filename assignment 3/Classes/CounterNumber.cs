801e36e0f1113aa85e7bebeceae9eb1f38e3eb53
abdka_wi5pcm7 <abdka_wi5pcm7@ABDALRAHMAN-HAM>
2026-02-22 00:25:38 +0300

    Add project files.

---
+++ assignment 3/Classes/CounterNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace assignment_3.Classes
+{
+    internal class Counter
+    {
+        private string str;
+        public Counter(string str)
+        {
+            this.str = str;
+        }
+        public void print()
+        {
+            Dictionary<char, int> dic = new Dictionary<char, int>();
+
+            foreach (var item in str)
+            {
+                if (dic.ContainsKey(item))
+                {
+                    dic[item]++;
+                }
+                else
+                {
+                    dic.Add(item, 1);
+                }
+            }
+            foreach (var item in dic)
+            {
+                Console.WriteLine($"{item.Key} : {item.Value}");
+            }
+        }
+    }
+}
---
+++ assignment 3/Program.cs
@@ -0,0 +1,5 @@
+﻿using assignment_3.Classes;
+Console.Write($"Pleaase Enter the word : ");
+string str = Console.ReadLine();
+Counter counter = new Counter(str);
+counter.print();
